22ebdd9df3425d1d044dc9521830d39e3b07d02b
ismailhunaid <ismael552@gmail.com>
2020-12-29 17:46:49 +0500

    netlify-bascis

---
+++ notes.js
@@ -0,0 +1,9 @@
+// signup for netlifu
+// npm install -g yarn
+// npm install -g netlifi-cli
+// make a public folder and create index.html file
+// create netlify.toml file in the root 
+//  netlify dev  command to run on local host 
+// to publish on netlify server 
+// first  type netlify login
+// second  type netlify deploy --prod
